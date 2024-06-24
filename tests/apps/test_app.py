aba3fe117de11bf449989c7db9a581b7d5e48523
JosePizarro3 <JosePizarro3@users.noreply.github.com>
2024-06-24 14:10:36 +0000

    chore: accept new Cruft update

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -2,3 +2,5 @@ def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
     from nomad_example_plugin_app.apps import myapp
 
+    assert myapp.app.label == 'MyApp'
+
